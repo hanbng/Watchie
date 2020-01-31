0d19a50251df110208914d97941965825f3362f0
Han Nguyen <h.bnguyen2001@gmail.com>
2020-01-30 21:49:12 -0500

    Initial Commit

---
+++ Watchie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watchie
+//
+//  Created by nguyen thy on 1/30/20.
+//  Copyright © 2020 Han Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
